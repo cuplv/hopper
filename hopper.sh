cdfe680e366777643041f84327dd6577e911b9de
Sam Blackshear <sam.blackshear@gmail.com>
2014-10-25 14:26:28 -0600

    Call correct scala runtime in hopper.sh

--- hopper.sh
+++ hopper.sh
@@ -10,6 +10,6 @@ SBT_CACHE=$HOME/.ivy2/cache
 
 CLASSPATH=.:target/scala-2.10/classes/:lib/com.microsoft.z3.jar:$MVN_CACHE/com/ibm/wala/com.ibm.wala.core/1.3.4-SNAPSHOT/com.ibm.wala.core-1.3.4-SNAPSHOT.jar:$MVN_CACHE/com/ibm/wala/com.ibm.wala.util/1.3.4-SNAPSHOT/com.ibm.wala.util-1.3.4-SNAPSHOT.jar:$MVN_CACHE/com/ibm/wala/com.ibm.wala.shrike/1.3.4-SNAPSHOT/com.ibm.wala.shrike-1.3.4-SNAPSHOT.jar:$SBT_CACHE/com.squareup/javawriter/jars/javawriter-2.5.0.jar:$SBT_CACHE/com.twitter/util-collection_2.10/jars/util-collection_2.10-6.12.1.jar:$SBT_CACHE/com.twitter/util-core_2.10/jars/util-core_2.10-6.12.1.jar:$SBT_CACHE/commons-collections/commons-collections/jars/commons-collections-3.2.1.jar:$SBT_CACHE/com.google.code.findbugs/jsr305/jars/jsr305-1.3.9.jar:org.$SBT_CACHE/org.scala-lang/scala-library/jars/scala-library-2.10.3.jar:$SBT_CACHE/com.google.guava/guava/bundles/guava-15.0.jar:$SBT_CACHE/local/University\ of\ Colorado\ Boulder/droidel_2.10/0.1-SNAPSHOT/jars/droidel_2.10.jar
 
-scala210 -cp "$CLASSPATH" -J-Xmx8g edu.colorado.hopper.driver.Main $@
+scala -cp "$CLASSPATH" -J-Xmx8g edu.colorado.hopper.driver.Main $@
 
 
