a7afb36022188e58897acf6597f3e7abe549e5d4
Benno Stein <benno.stein@gmail.com>
2016-02-02 10:16:38 -0700

    Enforce condition that MUSE alarms are at invocation sites

--- src/main/scala/edu/colorado/hopper/client/android/AndroidConstantFlowClient.scala
+++ src/main/scala/edu/colorado/hopper/client/android/AndroidConstantFlowClient.scala
@@ -17,48 +17,50 @@ class AndroidConstantFlowClient(appPath : String,
   def check : Unit = {
     Options.JUMPING_EXECUTION = true
     Options.PRINT_REFS = true
-    alarms.foreach{
-      case (iindex, nodeID, 1) => (checkBug1 _).tupled(getAlarmSite(iindex,nodeID))
-      case (iindex, nodeID, 2) => (checkBug2 _).tupled(getAlarmSite(iindex,nodeID))
-      case (iindex, nodeID, 3) => (checkBug3 _).tupled(getAlarmSite(iindex,nodeID))
-      case (iindex, nodeID, 4) => (checkBug4 _).tupled(getAlarmSite(iindex,nodeID))
-      case (iindex, nodeID, 5) => (checkBug5 _).tupled(getAlarmSite(iindex,nodeID))
-      case (iindex, nodeID, 6) => (checkBug6 _).tupled(getAlarmSite(iindex,nodeID))
+    alarms.foreach { case (iindex,nodeID,bugType) =>
+      println(s"__MUSE_CONSTANT_SEARCH__ Checking alarm {iindex : $iindex, cg_node_id : $nodeID, bugType : $bugType}")
+      bugType match {
+        case 1 => (checkBug1 _).tupled(getAlarmSite(iindex, nodeID))
+        case 2 => (checkBug2 _).tupled(getAlarmSite(iindex, nodeID))
+        case 3 => (checkBug3 _).tupled(getAlarmSite(iindex, nodeID))
+        case 4 => (checkBug4 _).tupled(getAlarmSite(iindex, nodeID))
+        case 5 => (checkBug5 _).tupled(getAlarmSite(iindex, nodeID))
+        case 6 => (checkBug6 _).tupled(getAlarmSite(iindex, nodeID))
+      }
+      println(s"__MUSE_CONSTANT_SEARCH__ Done checking alarm {iindex : $iindex, cg_node_id : $nodeID, bugType : $bugType}")
     }
   }
 
-  private def checkBug1(node : CGNode, i : SSAInstruction) : Unit = {
-    require(i.isInstanceOf[SSAInvokeInstruction])
-    val instr = i.asInstanceOf[SSAInvokeInstruction]
-    require(instr.getDeclaredTarget.getName.toString == "getInstance" && instr.getDeclaredTarget.getDeclaringClass.getName.getClassName.toString == "Cipher")
-    println(s"__MUSE_CONSTANT_SEARCH__ Checking alarm {iindex : ${instr.iindex}, nodeID : ${node.getGraphNodeId}, bugType : 1}")
-    val encryption_mode = instr.getUse(0)
+  private def checkBug1(node : CGNode, i : SSAInvokeInstruction) : Unit = {
+    require(i.getDeclaredTarget.getName.toString == "getInstance" && i.getDeclaredTarget.getDeclaringClass.getName.getClassName.toString == "Cipher")
+    val encryption_mode = i.getUse(0)
     if (node.getIR.getSymbolTable.isStringConstant(encryption_mode))
       println(s"__MUSE_CONSTANT_SEARCH__ Constant found : ${node.getIR.getSymbolTable.getStringValue(encryption_mode)}")
     else {
       val enc_mode_lpk = Var.makeLPK(encryption_mode, node, walaRes.hm)
       val enc_mode_pure = Pure.makePureVar(enc_mode_lpk)
-      val qry = Qry.make(List(PtEdge.make(enc_mode_lpk, enc_mode_pure)), instr, node, walaRes.hm)
+      val qry = Qry.make(List(PtEdge.make(enc_mode_lpk, enc_mode_pure)), i, node, walaRes.hm)
       qry.addPureConstraint(Pure.makeEqConstraint(enc_mode_pure, new StringVal("__MUSE_CONSTANT_SEARCH__")))
       if (exec.executeBackward(qry)) // ONLY OCCURS IN CASE OF TIMEOUT
         println("__MUSE_CONSTANT_SEARCH__ Search timed out")
       else
         println("__MUSE_CONSTANT_SEARCH__ Search completed")
     }
-    println(s"__MUSE_CONSTANT_SEARCH__ Done checking alarm {iindex : ${instr.iindex}, nodeID : ${node.getGraphNodeId}, bugType : 1}")
   }
-  private def checkBug2(node : CGNode, i : SSAInstruction) : Unit = {???}
-  private def checkBug3(node : CGNode, i : SSAInstruction) : Unit = {???}
-  private def checkBug4(node : CGNode, i : SSAInstruction) : Unit = {???}
-  private def checkBug5(node : CGNode, i : SSAInstruction) : Unit = {???}
-  private def checkBug6(node : CGNode, i : SSAInstruction) : Unit = {???}
+  private def checkBug2(node : CGNode, i : SSAInvokeInstruction) : Unit = {???}
+  private def checkBug3(node : CGNode, i : SSAInvokeInstruction) : Unit = {???}
+  private def checkBug4(node : CGNode, i : SSAInvokeInstruction) : Unit = {???}
+  private def checkBug5(node : CGNode, i : SSAInvokeInstruction) : Unit = {???}
+  private def checkBug6(node : CGNode, i : SSAInvokeInstruction) : Unit = {???}
 
 
-  def getAlarmSite(iindex : Int, nodeID : Int) : (CGNode, SSAInstruction) = {
+  def getAlarmSite(iindex : Int, nodeID : Int) : (CGNode, SSAInvokeInstruction) = {
     walaRes.cg.find {_.getGraphNodeId == nodeID}
       .flatMap {node =>
         node.getIR.iterateAllInstructions.find {_.iindex == iindex}
-          .map {instr => (node,instr)}
+          .map {instr =>
+            require(instr.isInstanceOf[SSAInvokeInstruction])
+            (node,  instr.asInstanceOf[SSAInvokeInstruction])}
       } match {
         case Some(site) => site
         case None => sys.error(s"Alarm site at iindex $iindex of node $nodeID not witnessed in call graph.")
